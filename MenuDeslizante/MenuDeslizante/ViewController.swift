00918a3220d1951c4de003b7bc342e20377484ef
sergio ivan lopez monzon <sergioivan154@gmail.com>
2015-11-17 14:52:43 -0600

    primer commit

---
+++ MenuDeslizante/MenuDeslizante/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuDeslizante
+//
+//  Created by sergio ivan lopez monzon on 17/11/15.
+//  Copyright (c) 2015 sergio ivan lopez monzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
