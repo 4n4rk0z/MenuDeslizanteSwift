0c07d450974a1f79b8843a5196b2a8e445b6dd23
sergio ivan lopez monzon <sergioivan154@gmail.com>
2015-11-29 12:23:25 -0600

    botones

---
+++ MenuDeslizante/MenuDeslizante/PageItemViralController.swift
@@ -0,0 +1,9 @@
+//
+//  PageItemViralController.swift
+//  MenuDeslizante
+//
+//  Created by sergio ivan lopez monzon on 28/11/15.
+//  Copyright © 2015 sergio ivan lopez monzon. All rights reserved.
+//
+
+import Foundation
