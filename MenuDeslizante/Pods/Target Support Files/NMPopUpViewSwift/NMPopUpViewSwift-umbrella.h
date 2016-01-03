2d84e727e7f61666a7ece951fd4f27b894a7fd48
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-01-03 16:12:05 -0600

    VentanaPopUp

---
+++ MenuDeslizante/Pods/Target Support Files/NMPopUpViewSwift/NMPopUpViewSwift-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import "OpenPay.h"
+#import "OPToken.h"
+#import "OPCard.h"
+#import "OPAddress.h"
+#import "DeviceCollectorSDK.h"
+
+FOUNDATION_EXPORT double NMPopUpViewSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char NMPopUpViewSwiftVersionString[];
+
