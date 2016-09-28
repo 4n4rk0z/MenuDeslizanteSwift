a4eea470907455ed1a3c1403d40c44f240db666e
4n4rk0z <billyjackbatesgarcia@l3sl13.local>
2016-09-28 03:11:29 -0500

    Ventana de login, cambios en popups y cambio en vies de tarjeta de credito y 7 dias de regalo

---
+++ MenuDeslizante/MenuDeslizante/formaPagoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  formaPagoViewController.swift
+//  MenuDeslizante
+//
+//  Created by Billy Jack Bates Garcia on 9/20/16.
+//  Copyright © 2016 sergio ivan lopez monzon. All rights reserved.
+//
+
+import UIKit
+
+class formaPagoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "fondo_pago")!.drawInRect(self.view.bounds)
+        var image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.view.backgroundColor = UIColor(patternImage: image)
+       
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
